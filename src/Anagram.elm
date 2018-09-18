59031797bcf8785adc8c3071e5ae013b92c2d6e1
Christopher Kolstad <christopher.kolstad@finn.no>
2018-09-18 13:02:32 +0200

    Make it look a tad better

--- src/Anagram.elm
+++ src/Anagram.elm
@@ -2,7 +2,9 @@ module Anagram exposing (main)
 
 import Browser
 import Debug
-import Html exposing (Attribute, Html, button, div, input, label, text)
+import Dict as D
+import Dict.Extra as E
+import Html exposing (Attribute, Html, button, div, fieldset, form, h2, input, label, legend, section, table, tbody, td, text, th, thead, tr)
 import Html.Attributes as A
 import Html.Events exposing (onClick, onInput)
 import Http
@@ -37,44 +39,111 @@ init _ =
 
 view : Model -> Html Msg
 view model =
+    div [ A.id "layout", A.class "pure-g" ]
+        [ sidebar model
+        , answer model
+        ]
+
+
+sidebar : Model -> Html Msg
+sidebar model =
     let
         wordCountStr =
-            model.minCount
-                |> String.fromInt
+            String.fromInt model.minCount
 
-        maxLength =
+        wordLength =
             model.word
                 |> String.length
                 |> String.fromInt
     in
-    div []
-        [ div []
-            [ div [] [ input [ A.placeholder "Find anagrams for?", A.value model.word, onInput ChangeWord ] [] ]
-            , div [] [ input [ A.type_ "number", A.value wordCountStr, A.min "2", A.max "6", onInput ChangeCount ] [] ]
-            , div [] [ button [ onClick PerformSearch ] [ text "Find anagrams" ] ]
+    div [ A.class "sidebar pure-u-1 pure-u-md-1-4" ]
+        [ div [ A.class "header" ]
+            [ h2 [ A.class "brand-tagline" ] [ text "Find your anagrams" ]
+            , fieldset []
+                [ legend [] [ text "Find anagrams for?" ]
+                , input [ A.placeholder "Baseword", A.value model.word, onInput ChangeWord ] []
+                , input [ A.type_ "number", A.value wordCountStr, A.min "2", A.max wordLength, onInput ChangeCount ] []
+                , button [ A.class "pure-button pure-button-primary", onClick PerformSearch ] [ text "Find anagrams" ]
+                ]
             ]
-        , div [] (anagramList model.anagrams)
         ]
 
 
-anagramNode : String -> Html Msg
-anagramNode anagram =
-    div [] [ text anagram ]
+answer : Model -> Html Msg
+answer model =
+    let
+        anagrams =
+            if List.isEmpty model.anagrams then
+                []
+
+            else
+                anagramGroups model
+    in
+    div [ A.class "content pure-u-1 pure-u-md-3-4" ]
+        [ div [] anagrams
+        ]
 
 
-anagramList : List String -> List (Html Msg)
-anagramList anagrams =
-    if List.isEmpty anagrams then
-        [ div [] [ text "No anagrams found yet" ] ]
+anagramGroups : Model -> List (Html Msg)
+anagramGroups model =
+    let
+        grouped =
+            E.groupBy String.length model.anagrams
 
-    else
-        let
-            sortByLengthAsc =
-                List.sortBy String.length anagrams
-        in
-        sortByLengthAsc
-            |> List.reverse
-            |> List.map anagramNode
+        wordLengths =
+            List.range model.minCount (String.length model.word)
+                |> List.reverse
+                |> List.map (getData grouped)
+    in
+    Debug.log "words" wordLengths
+        |> List.map wordSection
+
+
+getData : D.Dict Int (List String) -> Int -> List String
+getData d k =
+    case D.get k d of
+        Just h ->
+            List.sort h
+
+        Nothing ->
+            []
+
+
+wordSection : List String -> Html Msg
+wordSection words =
+    let
+        letterCount =
+            case List.head words of
+                Just h ->
+                    String.length h
+
+                Nothing ->
+                    0
+    in
+    section [ A.class "post" ]
+        [ table [ A.class "pure-table" ]
+            [ thead []
+                [ tr []
+                    [ th [] [ text "Number of letters" ]
+                    , th [] [ text (String.fromInt letterCount) ]
+                    ]
+                ]
+            , tbody [] (wordRows words)
+            ]
+        ]
+
+
+wordRows : List String -> List (Html Msg)
+wordRows words =
+    words
+        |> List.map word
+
+
+word : String -> Html Msg
+word w =
+    tr []
+        [ td [ A.colspan 2 ] [ text w ]
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -99,7 +168,7 @@ update msg model =
             ( { model | minCount = newCount }, Cmd.none )
 
         PerformSearch ->
-            ( model, getAnagrams model.word )
+            ( model, getAnagrams model )
 
         NewAnagrams anagramsList ->
             let
@@ -114,9 +183,9 @@ update msg model =
             ( { model | anagrams = anagrams }, Cmd.none )
 
 
-getAnagrams : String -> Cmd Msg
-getAnagrams word =
-    Http.send NewAnagrams (Http.get (toAnagramUrl word) anagramDecoder)
+getAnagrams : Model -> Cmd Msg
+getAnagrams model =
+    Http.send NewAnagrams (Http.get (toAnagramUrl model) anagramDecoder)
 
 
 anagramDecoder : Decode.Decoder (List String)
@@ -124,11 +193,12 @@ anagramDecoder =
     Decode.field "anagrams" (Decode.list Decode.string)
 
 
-toAnagramUrl : String -> String
-toAnagramUrl word =
-    U.crossOrigin "https://anagrambackend.herokuapp.com"
+toAnagramUrl : Model -> String
+toAnagramUrl model =
+    U.crossOrigin "https://anagrambackend.chriswk.com"
         [ "anagram" ]
-        [ U.string "word" word
+        [ U.string "word" model.word
+        , U.string "minCount" (String.fromInt model.minCount)
         ]
 
 
